c58a36271192a96cbaf83e278ea57d3f3df115ac
Swastik Panda <swastiksuman@gmail.com>
2019-08-14 23:55:16 +0530

    updated flow

--- src/main/java/com/vzw/prepaid/controller/ProcessController.java
+++ src/main/java/com/vzw/prepaid/controller/ProcessController.java
@@ -2,40 +2,25 @@ package com.vzw.prepaid.controller;
 
 import java.util.List;
 
+import org.activiti.engine.RuntimeService;
 import org.activiti.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.vzw.prepaid.service.ProcessService;
-
 @RestController
 public class ProcessController {
 
 	@Autowired
-	private ProcessService processService;
-
+	private RuntimeService runtimeService;
 	/*
 	 * Method will start the Activiti process engine and set employee to perform
 	 * the task
 	 */
 	@RequestMapping(value = "/process")
-	public String startProcessInstance(@RequestParam String assignee) {
-		return processService.startTheProcess(assignee);
+	public void startProcessInstance(@RequestParam String assignee) {
+		runtimeService.startProcessInstanceById("simple-process");
 	}
 
-	// Retrieve the tasks assigned to an employee
-	@RequestMapping(value = "/tasks")
-	public String getTasks(@RequestParam String assignee) {
-		List<Task> tasks = processService.getTasks(assignee);
-		return tasks.toString();
-	}
-
-	// Complete the task by their ID
-	@RequestMapping(value = "/completetask")
-	public String completeTask(@RequestParam String taskId) {
-		processService.completeTask(taskId);
-		return "Task with id " + taskId + " has been completed!";
-	}
 }
